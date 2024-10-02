2bc3ac3d34585b222322867a07ab61b7cdb95082
AztyUwU <benderpro102@gmail.com>
2024-10-02 14:06:36 +0200

    feat: Creation of the entities

---
+++ src/Enum/CommentStatusEnum.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+enum CommentStatusEnum: string
+{
+    case VALID = 'valid';
+    case PENDING = 'pending';
+    case REJECTED = 'rejected';
+    case DELETED = 'deleted';
+}
